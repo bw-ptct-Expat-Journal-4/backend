f5b83663d14bde0b0936467b01bf2f225d363b9b
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2020-05-28 18:02:42 -0500

    Updates

--- api/server.js
+++ api/server.js
@@ -15,15 +15,8 @@ server.use('/app/auth', authRouter);
 server.use('/app/users', usersRouter);
 server.use('/app/story', storyRouter)
 
-server.get('/', async (req, res) => {
-    try {
-      
-      const messageOfTheDay = process.env.MOTD || 'Hello World!'; // add this line
-      res.status(200).json({ motd: messageOfTheDay}); // change this line
-    } catch (error) {
-      console.error('\nERROR', error);
-      res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
-    }
-  });
+server.get('/', (req, res) => {
+    res.send("This Is The Server")
+})
 
 module.exports = server;
